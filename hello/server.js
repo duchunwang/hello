3a57f9bf9c186f14acd8f0594a7a89ae0a129e2c
duchunwang <4963636@qq.com>
2017-05-24 13:56:21 +0800

    test1

---
+++ hello/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const myapp = require('./app');
+const router = express.Router();
+
+// Request logger
+app.use(morgan('combined'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+
+myapp(router);
+
+// 转到子系统
+app.get('/', function(req,res){
+    res.send('Hello World!');
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(80);
+  console.log('Express started on port 80');
+}
